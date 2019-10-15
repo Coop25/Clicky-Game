c8518ca20be8726e6aa197d1a01e203979240d67
Cooper <frankidyfrank@gmail.com>
2019-10-15 17:01:18 -0500

    Updates

---
+++ precache-manifest.7ae24c19b9eb10bdf70d543f87f1035f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d445f93639f5090e33afb2de14a2bfc",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "40f6a1d882eee6752106",
+    "url": "/Clicky-Game/static/css/main.0d3364e6.chunk.css"
+  },
+  {
+    "revision": "88c8893309b260ef3696",
+    "url": "/Clicky-Game/static/js/2.03dc7dc3.chunk.js"
+  },
+  {
+    "revision": "40f6a1d882eee6752106",
+    "url": "/Clicky-Game/static/js/main.a9b93d83.chunk.js"
+  },
+  {
+    "revision": "06a49981db2d84194b2d",
+    "url": "/Clicky-Game/static/js/runtime-main.dc307434.js"
+  }
+]);
